bf20c1e5ae99a24222d3fe669472a035b170fa6c
elinaldo-santos <elinaldoahff@gmail.com>
2026-09-02 18:40:04 -0300

    logo

--- frontend/src/lib/queryClient.ts
+++ frontend/src/lib/queryClient.ts
@@ -5,7 +5,7 @@ export const queryClient = new QueryClient({
     queries: {
       staleTime: 5 * 60 * 1000,
       gcTime: 30 * 60 * 1000,
-      refetchInterval: 30 * 1000,
+      refetchInterval: 10 * 1000,
       refetchIntervalInBackground: false,
       refetchOnWindowFocus: false,
       retry: 1,
